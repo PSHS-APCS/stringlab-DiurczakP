7ac3a2bdb85732cd13c0639c9005f0b304400a28
DiurczakP <31449692+DiurczakP@users.noreply.github.com>
2018-03-04 13:27:50 -0600

    Add files via upload

--- TextLab08st.java
+++ TextLab08st.java
@@ -7,47 +7,39 @@ import java.util.Scanner;
 
 public class TextLab08st
 {
-	public static void main(String args[])
-	{
-		System.out.println("TextLab08\n\n");
-		FullName fn = new FullName();
-		System.out.println("\nFirst Name First: " + fn.firstNameFirst());
-		System.out.println("\nLast  Name First: " + fn.lastNameFirst());
-		System.out.println();
-	}
+    public static void main(String args[])
+    {
+        System.out.println("TextLab08\n\n");
+        FullName fn = new FullName();
+        System.out.println("\nFirst Name First: " + fn.firstNameFirst());
+        System.out.println("\nLast  Name First: " + fn.lastNameFirst());
+        System.out.println();
+    }
 }
 
 class FullName
 {
-	String name;
+    String name;
 
-	public FullName()
-	{
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter your full name, first name first.  -->  ");
-		name = input.nextLine();
-	}
+    public FullName()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your full name, first name first.  -->  ");
+        name = input.nextLine();
+    }
 
-	public String firstNameFirst()
-	{
-		return name;
-	}
-
-	public String lastNameFirst()
-	{
-		/*
-		 *  80 point precondition: The entered name consists of a First Name and a Last Name seprated by a single space.
-		 *
-		 * 100 point precondition: The entered name consists of a First Name, Last Name and any number of Middle Names.
-		 *                         NOTE: "any number" means it is possible not to have no Middle Name at all.
-		 *                         You can assume the user is not entering any extra spaces.
-		 */
-
-
-
-
-
-
-	}
+    public String firstNameFirst()
+    {
+    return name;
 }
 
+    public String lastNameFirst()
+    {
+        String firstName = name.substring(0, name.indexOf(" "));
+        String middleName = name.substring(firstName.length()+1, name.lastIndexOf(" "));
+        String lastName = name.substring(middleName.length()+firstName.length()+2, name.length());
+        return lastName +", " + firstName+" " + middleName;
+
+
+    }
+}
